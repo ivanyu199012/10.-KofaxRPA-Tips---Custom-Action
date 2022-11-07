43c83425e1a7815bc84413c882b694a0f6743897
ivanyu@xerox <chuenyuen027546@gmail.com>
2022-11-07 16:52:41 +0900

    개선
    1. getMsgWithCurrentDateTime 추가
    2. 외부 library 사용

--- NodeJs/test.js
+++ NodeJs/test.js
@@ -1,3 +1,4 @@
-const { isFileExists } = require( "fileHandler" );
+const { isFileExists, getMsgWithCurrentDateTime } = require("sampleHandler/sampleHandler.js");
 console.log( isFileExists( "C:\\MyFiles\\DocTemp\\LF Logistic\\PC1_Login.jpg" ) );
 console.log( isFileExists( "C:\\MyFiles\\DocTemp\\LF Logistic\\PC1_FALSE_Login.jpg" ) );
+console.log( getMsgWithCurrentDateTime( "Hello Guys ?" ) );
