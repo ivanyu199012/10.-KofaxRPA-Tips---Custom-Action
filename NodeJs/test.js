eabb92d134eb1689f2f32eac9ce765ab9555d794
ivanyu@xerox <chuenyuen027546@gmail.com>
2022-11-07 16:55:30 +0900

    Get Message with Current Date & Time function 추가

--- NodeJs/test.js
+++ NodeJs/test.js
@@ -1,4 +1,4 @@
-const { isFileExists, getMsgWithCurrentDateTime } = require("sampleHandler/sampleHandler.js");
+const { isFileExists, getMsgWithCurrentDateTime } = require("sampleHandler/sampleHandler");
 console.log( isFileExists( "C:\\MyFiles\\DocTemp\\LF Logistic\\PC1_Login.jpg" ) );
 console.log( isFileExists( "C:\\MyFiles\\DocTemp\\LF Logistic\\PC1_FALSE_Login.jpg" ) );
 console.log( getMsgWithCurrentDateTime( "Hello Guys ?" ) );
